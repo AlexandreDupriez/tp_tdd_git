affe52a22db15b1892d3ca6ea7745b297dafc248
Alexandre Dupriez <alexdup59@hotmail.fr>
2015-03-31 10:42:20 +0200

    V_1à16

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -1,10 +1,59 @@
 package iut.tdd;
 
+import java.util.HashMap;
+
 public class Convert {
+	
+	
+	static HashMap <String,String> num = new HashMap<String, String>(); 
+
 	public static String num2text(String input) {
-		return null;
-	}
+		
+		num.put("zero", "0");
+		num.put("un", "1");
+		num.put("deux", "2");
+		num.put("trois", "3");
+		num.put("quatre", "4");
+		num.put("cinq", "5");
+		num.put("six", "6");
+		num.put("sept", "7");
+		num.put("huit", "8");
+		num.put("neuf", "9");
+		num.put("dix", "10");
+		num.put("onze", "11");
+		num.put("douze", "12");
+		num.put("treize", "13");
+		num.put("quatorze", "14");
+		num.put("quinze", "15");
+		num.put("seize", "16");
+		
+		
+
+		return num.get(input);
+	}	
+	
+	
 	public static String text2num(String input) {
-		return null;
+
+		num.put("0", "zero");
+		num.put("1", "un");
+		num.put("2", "deux");
+		num.put("3", "trois");
+		num.put("4", "quatre");
+		num.put("5", "cinq");
+		num.put("6", "six");
+		num.put("7", "sept");
+		num.put("8", "huit");
+		num.put("9", "neuf");
+		num.put("10", "dix");
+		num.put("11", "onze");
+		num.put("12", "douze");
+		num.put("13", "treize");
+		num.put("14", "quatorze");
+		num.put("15", "quinze");
+		num.put("16", "seize");
+		
+		return num.get(input);
+		
 	}
 }
--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -7,14 +7,48 @@ import org.junit.Test;
 public class TestConvert {
 	@Test
 	public void test_num2text_zero () {
-		// Given
-		String input = "0";
-		String expected = "zéro";
-		Convert myConvert = new Convert();
-		// When
-		String actual = myConvert.num2text(input);
-		// Then
-		Assert.assertEquals(expected, actual);
+		
+		Assert.assertEquals("0", Convert.num2text("zero"));
+		Assert.assertEquals("1", Convert.num2text("un"));
+		Assert.assertEquals("2", Convert.num2text("deux"));
+		Assert.assertEquals("3", Convert.num2text("trois"));
+		Assert.assertEquals("4", Convert.num2text("quatre"));
+		Assert.assertEquals("5", Convert.num2text("cinq"));
+		Assert.assertEquals("6", Convert.num2text("six"));
+		Assert.assertEquals("7", Convert.num2text("sept"));
+		Assert.assertEquals("8", Convert.num2text("huit"));
+		Assert.assertEquals("9", Convert.num2text("neuf"));
+		Assert.assertEquals("10", Convert.num2text("dix"));
+		Assert.assertEquals("11", Convert.num2text("onze"));
+		Assert.assertEquals("12", Convert.num2text("douze"));
+		Assert.assertEquals("13", Convert.num2text("treize"));
+		Assert.assertEquals("14", Convert.num2text("quatorze"));
+		Assert.assertEquals("15", Convert.num2text("quinze"));
+		Assert.assertEquals("16", Convert.num2text("seize"));
+		
+		
 	}
+	@Test
+	public void test_text2num_zero () {
 	
+		Assert.assertEquals("zero", Convert.text2num("0"));
+		Assert.assertEquals("un", Convert.text2num("1"));
+		Assert.assertEquals("deux", Convert.text2num("2"));
+		Assert.assertEquals("trois", Convert.text2num("3"));
+		Assert.assertEquals("quatre", Convert.text2num("4"));
+		Assert.assertEquals("cinq", Convert.text2num("5"));
+		Assert.assertEquals("six", Convert.text2num("6"));
+		Assert.assertEquals("sept", Convert.text2num("7"));
+		Assert.assertEquals("huit", Convert.text2num("8"));
+		Assert.assertEquals("neuf", Convert.text2num("9"));
+		Assert.assertEquals("dix", Convert.text2num("10"));
+		Assert.assertEquals("onze", Convert.text2num("11"));
+		Assert.assertEquals("douze", Convert.text2num("12"));
+		Assert.assertEquals("treize", Convert.text2num("13"));
+		Assert.assertEquals("quatorze", Convert.text2num("14"));
+		Assert.assertEquals("quinze", Convert.text2num("15"));
+		Assert.assertEquals("seize", Convert.text2num("16"));
+		
+		
+	}
 }
